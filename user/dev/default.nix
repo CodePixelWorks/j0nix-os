089559e88565f54bb021cbe179fcd60bba2ff631
Jonas Tschoche <me@codepixelstudio.de>
2026-02-20 17:40:13 +0100

    feat(dev): add configurable git and ssh host profiles

--- user/dev/default.nix
+++ user/dev/default.nix
@@ -2,6 +2,65 @@
 let
   dev = settings.dev or { };
   enabled = dev.enable or true;
+  gitCfg = dev.git or { };
+  gitEnabled = gitCfg.enable or true;
+  gitUserName = gitCfg.userName or (settings.name or settings.username);
+  gitUserEmail = gitCfg.userEmail or (settings.email or "${settings.username}@localhost");
+  gitDefaultBranch = gitCfg.defaultBranch or "main";
+  gitHostProfiles = gitCfg.hostProfiles or { };
+
+  sshCfg = dev.ssh or { };
+  sshEnabled = sshCfg.enable or true;
+  sshAddKeysToAgent = sshCfg.addKeysToAgent or "yes";
+
+  mkGitHostInclude = name: profile:
+    let
+      profileUserName = profile.userName or gitUserName;
+      profileUserEmail = profile.userEmail or gitUserEmail;
+    in
+    {
+      name = "git/hosts/${name}.inc";
+      value.text = ''
+        [user]
+          name = ${profileUserName}
+          email = ${profileUserEmail}
+      '';
+    };
+
+  mkGitIncludes = name: profile:
+    let
+      host = profile.host or name;
+      includePath = "~/.config/git/hosts/${name}.inc";
+    in
+    [
+      {
+        condition = "hasconfig:remote.*.url:git@${host}:**";
+        path = includePath;
+      }
+      {
+        condition = "hasconfig:remote.*.url:https://${host}/**";
+        path = includePath;
+      }
+    ];
+
+  mkSshMatchBlock = name: profile:
+    let
+      host = profile.host or name;
+      sshProfile = profile.ssh or { };
+    in
+    {
+      name = name;
+      value =
+        {
+          host = sshProfile.match or host;
+          hostname = host;
+          user = sshProfile.user or "git";
+          identitiesOnly = sshProfile.identitiesOnly or false;
+        }
+        // lib.optionalAttrs (sshProfile ? identityFile) {
+          identityFile = sshProfile.identityFile;
+        };
+    };
 in
 {
   imports = [
@@ -12,6 +71,25 @@ in
     programs.direnv = {
       enable = true;
       nix-direnv.enable = true;
+    };
+
+    programs.git = lib.mkIf gitEnabled {
+      enable = true;
+      userName = gitUserName;
+      userEmail = gitUserEmail;
+      extraConfig = {
+        init.defaultBranch = gitDefaultBranch;
+        pull.rebase = false;
+      };
+      includes = lib.flatten (lib.mapAttrsToList mkGitIncludes gitHostProfiles);
+    };
+
+    xdg.configFile = lib.mkIf gitEnabled (builtins.listToAttrs (lib.mapAttrsToList mkGitHostInclude gitHostProfiles));
+
+    programs.ssh = lib.mkIf sshEnabled {
+      enable = true;
+      addKeysToAgent = sshAddKeysToAgent;
+      matchBlocks = builtins.listToAttrs (lib.mapAttrsToList mkSshMatchBlock gitHostProfiles);
     };
 
     home.packages = with pkgs; [
