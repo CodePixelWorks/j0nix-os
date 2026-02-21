b763268b277cbf79182452c8e4bd910de806e793
Jonas Tschoche <me@codepixelstudio.de>
2026-02-21 16:22:52 +0100

    refactor(wm): use per-user defaultWMS with global wm install list

--- user/session-default.nix
+++ user/session-default.nix
@@ -1,12 +1,19 @@
 { lib, settings, ... }:
 let
   useUWSM = (settings.hyprland or { }).useUWSM or true;
+  defaultWMS = settings.defaultWMS or null;
   defaultSession =
     settings.defaultSession
     or (
-      if builtins.elem "hyprland" settings.wms then
+      if defaultWMS == "hyprland" then
         (if useUWSM then "hyprland-uwsm" else "hyprland")
-      else if builtins.elem "gnome" settings.wms then
+      else if defaultWMS == "gnome" then
+        "gnome"
+      else if defaultWMS == "mangowc" then
+        "mangowc"
+      else if builtins.elem "hyprland" (settings.wms or [ ]) then
+        (if useUWSM then "hyprland-uwsm" else "hyprland")
+      else if builtins.elem "gnome" (settings.wms or [ ]) then
         "gnome"
       else
         null
