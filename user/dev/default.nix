4fc787eeccf43971d444b62d2e9923c961960c35
Jonas Tschoche <me@codepixelstudio.de>
2026-02-26 04:55:43 +0100

    fix(ssh): make home-manager ssh defaults explicit
    - disable Home Manager's deprecated implicit SSH defaults\n- add explicit '*' match block values equivalent to previous defaults\n- preserve optional addKeysToAgent override from settings

--- user/dev/default.nix
+++ user/dev/default.nix
@@ -90,11 +90,21 @@ in
 
     programs.ssh = lib.mkIf sshEnabled {
       enable = true;
+      enableDefaultConfig = false;
       matchBlocks =
         (builtins.listToAttrs (lib.mapAttrsToList mkSshMatchBlock gitHostProfiles))
-        // lib.optionalAttrs (sshAddKeysToAgent != null) {
+        // {
           "*" = {
-            addKeysToAgent = sshAddKeysToAgent;
+            forwardAgent = false;
+            addKeysToAgent = if sshAddKeysToAgent != null then sshAddKeysToAgent else "no";
+            compression = false;
+            serverAliveInterval = 0;
+            serverAliveCountMax = 3;
+            hashKnownHosts = false;
+            userKnownHostsFile = "~/.ssh/known_hosts";
+            controlMaster = "no";
+            controlPath = "~/.ssh/master-%r@%n:%p";
+            controlPersist = "no";
           };
         };
     };
