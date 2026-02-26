c3546cab04fbdac119b049b9c3a6ad3b6799663f
Jonas Tschoche <me@codepixelstudio.de>
2026-02-26 09:44:31 +0100

    refactor(storage): move remount guards to shared mount helper
    - keep protected mount source-of-truth in the desktop profile via an internal managed mount list\n- add shared helper lib that derives systemd mount/automount unit guards from mountPoint + preventRemount\n- apply the games disk NTFS rebuild protection through the new generic path

---
+++ system/lib/mount-rebuild-guards.nix
@@ -0,0 +1,30 @@
+{ lib, utils }:
+mounts:
+let
+  normalizedMounts = lib.filter (m:
+    (m.enable or true)
+    && (m.preventRemount or false)
+    && (m ? mountPoint)
+    && m.mountPoint != ""
+  ) mounts;
+
+  mountUnitName = mountPoint: "${utils.escapeSystemdPath (lib.removeSuffix \"/\" mountPoint)}.mount";
+  automountUnitName = mountPoint: "${utils.escapeSystemdPath (lib.removeSuffix \"/\" mountPoint)}.automount";
+
+  mkNoRemountUnit = unitName: {
+    name = unitName;
+    value = {
+      stopIfChanged = false;
+      restartIfChanged = false;
+    };
+  };
+in
+builtins.listToAttrs (
+  lib.concatMap
+    (m:
+      [ (mkNoRemountUnit (mountUnitName m.mountPoint)) ]
+      ++ lib.optionals (m.automount or false) [
+        (mkNoRemountUnit (automountUnitName m.mountPoint))
+      ])
+    normalizedMounts
+)
