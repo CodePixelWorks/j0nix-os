abc39f74a879037e1f89e8b0bca225b340e69d93
Jonas Tschoche <me@codepixelstudio.de>
2026-03-04 21:53:20 +0100

    refactor(software): centralize home packages through aggregator
    - move desktop base package set from profiles/desktop/home.nix into user/software/desktop-base.nix
    - switch niri, mangowc, and betterdiscord modules from direct home.packages to j0nix.user.software.packages
    - add assertion guard in user/software/default.nix to fail when repo modules define home.packages directly
    - keep profile home module focused on session, mime, and UI behavior instead of package installation

---
+++ user/software/desktop-base.nix
@@ -0,0 +1,98 @@
+{ lib, pkgs, settings, ... }:
+let
+  syncthingCfg = (settings.programs or { }).syncthing or { };
+  syncthingEnabled = syncthingCfg.enable or false;
+  enableUdiskieAutomount = true;
+
+  configuredFileManagersRaw =
+    settings.fileManagers
+    or (lib.optional ((settings.preferredFileManager or null) != null) settings.preferredFileManager);
+  configuredFileManagers =
+    lib.unique (if configuredFileManagersRaw != [ ] then configuredFileManagersRaw else [ "nautilus" ]);
+  fileManagerPackage = name:
+    if name == "nautilus" then
+      pkgs.nautilus
+    else if name == "nemo" then
+      pkgs.nemo
+    else if name == "dolphin" then
+      if (pkgs ? kdePackages) && (pkgs.kdePackages ? dolphin) then pkgs.kdePackages.dolphin else null
+    else if name == "thunar" then
+      if (pkgs ? xfce) && (pkgs.xfce ? thunar) then pkgs.xfce.thunar else null
+    else
+      null;
+  fileManagerPackages = lib.filter (pkg: pkg != null) (map fileManagerPackage configuredFileManagers);
+
+  iconThemeCfg = settings.iconTheme or { };
+  iconThemeEnabled = iconThemeCfg.enable or true;
+  iconThemePackageKey = iconThemeCfg.package or "papirus";
+  iconThemePackage =
+    if iconThemePackageKey == "papirus" then
+      pkgs.papirus-icon-theme
+    else if iconThemePackageKey == "colloid" then
+      if pkgs ? "colloid-icon-theme" then
+        pkgs."colloid-icon-theme"
+      else
+        null
+    else if iconThemePackageKey == "adwaita" then
+      pkgs.adwaita-icon-theme
+    else if iconThemePackageKey == "breeze" then
+      if (pkgs ? kdePackages) && (pkgs.kdePackages ? breeze-icons) then
+        pkgs.kdePackages.breeze-icons
+      else if pkgs ? breeze-icons then
+        pkgs.breeze-icons
+      else
+        null
+    else
+      null;
+  iconThemeFallbackPackages = with pkgs; [
+    hicolor-icon-theme
+    adwaita-icon-theme
+  ];
+in
+{
+  j0nix.user.software.packages =
+    (with pkgs; [
+      foot
+      kitty
+      git
+      gh
+      starship
+      eza
+      bat
+      fd
+      ripgrep
+      tree
+      jq
+      obs-studio
+      qbittorrent
+      telegram-desktop
+      nextcloud-client
+      obsidian
+      drawio
+      bottles
+      simplescreenrecorder
+      gpu-screen-recorder
+      gpu-screen-recorder-gtk
+      krita
+      blender
+      gimp
+      naps2
+      mpv
+      libreoffice-fresh
+      gcc
+      gnumake
+      nodejs
+      python3
+      cargo
+      rustc
+      openvpn
+      unzip
+      android-tools
+      xdg-utils
+    ])
+    ++ fileManagerPackages
+    ++ lib.optionals enableUdiskieAutomount [ pkgs.udiskie ]
+    ++ lib.optionals syncthingEnabled [ pkgs.syncthing ]
+    ++ lib.optionals (pkgs ? fusion360) [ pkgs.fusion360 ]
+    ++ lib.optionals (iconThemeEnabled && iconThemePackage != null) ([ iconThemePackage ] ++ iconThemeFallbackPackages);
+}
