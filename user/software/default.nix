f1bcdc1dd03c1ee56ee8a65125c23fbb56bfc355
Jonas Tschoche <me@codepixelstudio.de>
2026-03-04 21:53:20 +0100

    refactor(software): centralize home packages through aggregator
    - move desktop base package set from profiles/desktop/home.nix into user/software/desktop-base.nix
    - switch niri, mangowc, and betterdiscord modules from direct home.packages to j0nix.user.software.packages
    - add assertion guard in user/software/default.nix to fail when repo modules define home.packages directly
    - keep profile home module focused on session, mime, and UI behavior instead of package installation

--- user/software/default.nix
+++ user/software/default.nix
@@ -1,8 +1,31 @@
-{ config, lib, ... }:
+{ config, lib, options, ... }:
 let
   cfg = config.j0nix.user.software;
+  repoRoot = lib.removeSuffix "/user/software" (toString ./.);
+  localModuleDefinitions =
+    lib.filter
+      (def:
+        let
+          file = toString (def.file or "");
+        in
+          file != "" && lib.hasPrefix "${repoRoot}/" file)
+      (options.home.packages.definitionsWithLocations or [ ]);
+  disallowedHomePackagesDefinitions =
+    lib.filter
+      (def:
+        let
+          file = toString (def.file or "");
+        in
+          !(lib.hasSuffix "/user/software/default.nix" file))
+      localModuleDefinitions;
+  disallowedHomePackagesFiles =
+    lib.unique (map (def: toString (def.file or "<unknown>")) disallowedHomePackagesDefinitions);
 in
 {
+  imports = [
+    ./desktop-base.nix
+  ];
+
   options.j0nix.user.software.packages = lib.mkOption {
     type = lib.types.listOf lib.types.package;
     default = [ ];
@@ -11,5 +34,15 @@ in
 
   config = {
     home.packages = lib.mkAfter (lib.unique cfg.packages);
+    assertions = [
+      {
+        assertion = disallowedHomePackagesDefinitions == [ ];
+        message = ''
+          Do not define home.packages directly in repository modules.
+          Use j0nix.user.software.packages instead.
+          Offending module file(s): ${lib.concatStringsSep ", " disallowedHomePackagesFiles}
+        '';
+      }
+    ];
   };
 }
