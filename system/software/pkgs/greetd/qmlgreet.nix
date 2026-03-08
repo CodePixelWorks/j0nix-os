beb13c3dd51f030695cf302c30ed6cf67ad1bb98
Jonas Tschoche <me@codepixelstudio.de>
2026-03-08 18:41:33 +0100

    feat(packages): add qmlgreet package based on mauikit4
    - package Nitrux qmlgreet from a pinned upstream commit via meson
    - install bundled default qmlgreet config and color scheme assets into share/qmlgreet
    - expose package through localFixesOverlay as pkgs.qmlgreet for greetd integration

---
+++ system/software/pkgs/greetd/qmlgreet.nix
@@ -0,0 +1,61 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  cmake,
+  meson,
+  ninja,
+  pkg-config,
+  qt6,
+  mauikit4,
+  wayland,
+  wayland-scanner,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "qmlgreet";
+  version = "unstable-2026-03-08";
+
+  src = fetchFromGitHub {
+    owner = "Nitrux";
+    repo = "qmlgreet";
+    rev = "8a870dd17742e18d5571a690b9c7f2b8166dbbc5";
+    hash = "sha256-lAXE8a8OhMz1SrobRb0ICTF77bQHEYv2ya+S4HC8gs0=";
+  };
+
+  nativeBuildInputs = [
+    meson
+    ninja
+    pkg-config
+    cmake
+    qt6.wrapQtAppsHook
+    wayland-scanner
+  ];
+
+  buildInputs = [
+    mauikit4
+    qt6.qt5compat
+    qt6.qtbase
+    qt6.qtdeclarative
+    qt6.qtwayland
+    wayland
+  ];
+
+  qtWrapperArgs = [
+    "--prefix QML2_IMPORT_PATH : ${mauikit4}/lib/qt-6/qml"
+  ];
+
+  postInstall = ''
+    install -Dm644 "$src/qmlgreet.conf" "$out/share/qmlgreet/qmlgreet.conf"
+    install -Dm644 "$src/QMLGreetDefault.colors" "$out/share/qmlgreet/QMLGreetDefault.colors"
+  '';
+
+  meta = {
+    description = "QML-based greeter for greetd and Wayland compositors";
+    homepage = "https://github.com/Nitrux/qmlgreet";
+    license = lib.licenses.bsd3;
+    maintainers = [ ];
+    mainProgram = "qmlgreet";
+    platforms = lib.platforms.linux;
+  };
+})
