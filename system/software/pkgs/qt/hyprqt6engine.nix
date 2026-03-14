044652b349143e81263a440160f909b7e6780eef
Jonas Tschoche <me@codepixelstudio.de>
2026-03-14 13:21:21 +0100

    feat(desktop): adopt hyprqt6engine for Qt theming
    - package the Hyprland Qt platform theme locally so the repo does not depend on nixpkgs carrying it
    - switch the default Hyprland session Qt platform theme to hyprqt6engine
    - keep the old qtengine setting as a compatibility alias and seed a minimal Hypr theme config

---
+++ system/software/pkgs/qt/hyprqt6engine.nix
@@ -0,0 +1,62 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+, cmake
+, ninja
+, pkg-config
+, hyprlang
+, hyprutils
+, libGL
+, libxkbcommon
+, kdePackages
+, qt6
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "hyprqt6engine";
+  version = "0.1.0";
+
+  src = fetchFromGitHub {
+    owner = "hyprwm";
+    repo = "hyprqt6engine";
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-Nw0xP54+85alddbU8lFebA3KY1vz8WkujDY9bA5K0MI=";
+  };
+
+  nativeBuildInputs = [
+    cmake
+    ninja
+    pkg-config
+    qt6.wrapQtAppsHook
+    qt6.qttools
+  ];
+
+  buildInputs = [
+    hyprlang
+    hyprutils
+    libGL
+    libxkbcommon
+    kdePackages.kcolorscheme
+    kdePackages.kconfig
+    kdePackages.kiconthemes
+    qt6.qt5compat
+    qt6.qtbase
+    qt6.qtsvg
+    qt6.qtwayland
+  ];
+
+  postPatch = ''
+    substituteInPlace common/config/ConfigManager.cpp \
+      --replace-fail '/etc' '${placeholder "out"}/etc'
+    substituteInPlace common/common.cpp \
+      --replace-fail '/usr/share' '${placeholder "out"}/share'
+  '';
+
+  meta = with lib; {
+    description = "Qt 6 platform theme and style provider for Hyprland";
+    homepage = "https://github.com/hyprwm/hyprqt6engine";
+    license = licenses.bsd3;
+    platforms = platforms.linux;
+    maintainers = [ ];
+  };
+})
