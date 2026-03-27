f8e447b0803136cdcd06326e0e39e3cf5e2ee613
Jonas Tschoche <me@codepixelstudio.de>
2026-03-27 20:24:25 +0100

    fix(home): silence HM 26.05 deprecation warnings
    - Set gtk.gtk4.theme = config.gtk.theme explicitly
    - Set xdg.userDirs.setSessionVariables = true explicitly
    - Preserves legacy behavior until stateVersion bump to 26.05

--- user/desktop/identity.nix
+++ user/desktop/identity.nix
@@ -1,10 +1,13 @@
-{ settings, ... }:
+{ config, settings, ... }:
 {
   home = {
     username = settings.username;
     homeDirectory = "/home/${settings.username}";
     stateVersion = "25.11";
   };
+
+  gtk.gtk4.theme = config.gtk.theme;
+  xdg.userDirs.setSessionVariables = true;
 
   programs.home-manager.enable = true;
 }
