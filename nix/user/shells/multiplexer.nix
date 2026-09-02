a08831a1e3d5a6f777923c3b4aa4af75badcdd50
Jonas Tschoche <me@codepixelstudio.de>
2026-09-02 08:55:08 +0200

    fix(shells): stop launching every shell inside tmux

--- nix/user/shells/multiplexer.nix
+++ nix/user/shells/multiplexer.nix
@@ -2,12 +2,6 @@
 let
   cfg = settings.terminalMultiplexer or { };
   enabled = cfg.enable or false;
-  command = cfg.command or "tmux";
-  startMultiplexer = ''
-    if [ -n "$PS1" ] && [ -z "''${TMUX:-}" ] && [ "''${TERM:-}" != "dumb" ] && command -v ${command} >/dev/null 2>&1; then
-      exec ${command} new-session
-    fi
-  '';
 in
 {
   j0nix.user.software.packages = lib.mkIf enabled [ pkgs.tmux ];
@@ -15,17 +9,4 @@ in
   programs.tmux = lib.mkIf enabled {
     enable = true;
   };
-
-  programs.bash = lib.mkIf enabled {
-    enable = true;
-    initExtra = startMultiplexer;
-  };
-
-  programs.zsh.initContent = lib.mkIf enabled (lib.mkBefore startMultiplexer);
-
-  programs.fish.interactiveShellInit = lib.mkIf enabled (lib.mkBefore ''
-    if status is-interactive; and test -z "$TMUX"; and test "$TERM" != dumb; and command -q ${command}
-      exec ${command} new-session
-    end
-  '');
 }
