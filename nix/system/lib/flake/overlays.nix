b0964f46489be5091ce0a6e4ca98a51e2489154b
Jonas Tschoche <me@codepixelstudio.de>
2026-09-07 12:41:01 +0200

    feat(gaming): add AMUMSS build runner
    - Pin AMUMSS and run Lua mod builds through Wine\n- Deploy current NMS PAKs into their GAMEDATA/MODS profile directory\n- Keep generated PAK activation and rollback under nms-patcher ownership

--- nix/system/lib/flake/overlays.nix
+++ nix/system/lib/flake/overlays.nix
@@ -34,6 +34,7 @@ let
     donsetch = final.callPackage (baseDir + "/nix/system/software/pkgs/dev/donsetch.nix") { };
     nexus-collection-dl = final.callPackage (baseDir + "/nix/system/software/pkgs/gaming/nexus-collection-dl.nix") { };
     nms-patcher = final.callPackage (baseDir + "/nix/system/software/pkgs/gaming/nms-patcher.nix") { };
+    nms-amumss = final.callPackage (baseDir + "/nix/system/software/pkgs/gaming/nms-amumss.nix") { };
     openldap = prev.openldap.overrideAttrs (_: {
       doCheck = false;
       doInstallCheck = false;
