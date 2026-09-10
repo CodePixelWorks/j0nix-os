f0524b784febb8acfa227162360d6ee65f482318
Jonas Tschoche <me@codepixelstudio.de>
2026-09-10 22:48:14 +0200

    fix(bottles): disable warning popup in package override
    Use the Bottles package variant with removeWarningPopup enabled in both gaming and Windows EXE integrations.

--- nix/user/gaming/launchers.nix
+++ nix/user/gaming/launchers.nix
@@ -17,7 +17,7 @@ let
   protonCfg = gaming.proton or { };
   minecraftCfg = (settings.programs or { }).minecraft or { };
   minecraftDataDir = minecraftCfg.dataDir or null;
-  bottlesPkg = pkgs.bottles;
+  bottlesPkg = pkgs.bottles.override { removeWarningPopup = true; };
   rockstarEnabled = launchers.rockstar or false;
   gdlauncherEnabled = launchers.gdlauncher or true;
   teamspeak6Enabled = launchers.teamspeak6 or true;
